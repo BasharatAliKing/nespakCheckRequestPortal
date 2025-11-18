20e96e416e05342fc98949c0b3c6ba51ba983a98
Basharat Ali <80580890+BasharatAliKing@users.noreply.github.com>
2025-11-18 12:40:01 +0500

    upfdfd

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 const contractorFormRouter = require('./routes/contractorFormRouter');
 const clientRouter = require('./routes/clientRouter');
 const contractorRouter = require('./routes/contractorRouter');
